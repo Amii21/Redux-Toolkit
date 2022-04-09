d33748a0a17d3f16c9453c032da3ff74b8c4990e
amit singh <amitsingh211196@gmail.com>
2022-04-09 12:57:17 +0530

    create,update & delete features added in crud app of redux toolkit

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,7 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { createPost } from "../redux/features/PostSlice";
+import Spinner from "./Spinner";
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  const [values, setValues] = useState({ title: "", body: "" });
+  const [showPost, setShowPost] = useState(false);
+  const { loading, post } = useSelector((state) => ({ ...state.app }));
+  const { title, body } = values;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createPost({ values }));
+    setValues({ title: "", body: "" });
+    setShowPost(true);
+  };
+
+  const showCreatedPost = () => {
+    return (
+      <>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div className="card mt-4">
+            <div className="card-body">
+              <h5 className="card-title">{post[0].title}</h5>
+              <p className="card-text">{post[0].body}</p>
+            </div>
+          </div>
+        )}
+      </>
+    );
+  };
+
+  return (
+    <div>
+      <h1 className="text-center bg-dark text-white p-2 mt-1">Create Post</h1>
+      <form>
+        <div className="mb-3 mt-4">
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setValues({ ...values, title: e.target.value })}
+            placeholder="Enter Post Title"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="form-floating">
+          <textarea
+            className="form-control"
+            value={body}
+            onChange={(e) => setValues({ ...values, body: e.target.value })}
+            // placeholder="Add Post Description"
+            id="floatingTextarea"
+          ></textarea>
+          <label for="floatingTextarea">Add Post Description</label>
+        </div>
+        <div className="mt-4 d-flex align-items-end justify-content-end">
+          <button className="btn btn-primary" onClick={() => navigate("/")}>
+            Go Home
+          </button>
+          <button
+            type="submit"
+            className="btn btn-danger ms-4"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+      <div className="mt-4">{showPost && <div>{showCreatedPost()}</div>}</div>
+    </div>
+  );
 };
 
 export default CreatePost;
--- src/redux/features/PostSlice.js
+++ src/redux/features/PostSlice.js
@@ -1,10 +1,54 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import { toHaveFormValues } from "@testing-library/jest-dom/dist/matchers";
 
-export const getPost = createAsyncThunk("post/getPosts", async ({ id }) => {
+export const getPost = createAsyncThunk("post/getPost", async ({ id }) => {
   return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) =>
     res.json()
   );
 });
+
+export const deletePost = createAsyncThunk(
+  "post/deletePost",
+  async ({ id }) => {
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+      method: "DELETE",
+    }).then((res) => res.json());
+  }
+);
+
+export const createPost = createAsyncThunk(
+  "post/createPost",
+  async ({ values }) => {
+    return fetch(`https://jsonplaceholder.typicode.com/posts`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        title: values.title,
+        body: values.body,
+      }),
+    }).then((res) => res.json());
+  }
+);
+
+export const updatePost = createAsyncThunk(
+  "post/updatePost",
+  async ({ id, title, body }) => {
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        body,
+      }),
+    }).then((res) => res.json());
+  }
+);
 
 const PostSlice = createSlice({
   name: "post",
@@ -12,6 +56,14 @@ const PostSlice = createSlice({
     loading: false,
     post: [],
     error: null,
+    body: "",
+    edit: false,
+  },
+  reducers: {
+    setEdit: (state, action) => {
+      state.body = action.payload.body;
+      state.edit = action.payload.edit;
+    },
   },
   extraReducers: {
     [getPost.pending]: (state, action) => {
@@ -25,7 +77,42 @@ const PostSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+    [deletePost.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [deletePost.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.post = [action.payload];
+    },
+    [deletePost.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    [createPost.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [createPost.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.post = [action.payload];
+    },
+    [createPost.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    [updatePost.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [updatePost.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.post = [action.payload];
+    },
+    [updatePost.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
+
+export const { setEdit } = PostSlice.actions;
 
 export default PostSlice.reducer;
